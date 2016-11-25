33390fa1cb93e249099063e144f3a9abe3686c5f
jogibear9988 <jochen.kuehner@gmx.de>
2016-11-25 14:57:47 +0100

    html5 typescript sample (WIP)

---
+++ Samples/Html5TypeScriptInterlinqSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Html5TypeScriptInterlinqSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Todo
+             * 
+             * db zugriff
+             * dto's
+             * model
+             * mapping
+             * migration?
+             * daten?
+             * interlinq server
+             * webserver
+             * 
+             * ts compile
+             * babelcompile
+             * 
+             * 
+             * 
+             */
+        }
+    }
+}
